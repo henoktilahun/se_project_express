4b6b166b6e465b5830322c3423d9e73c7c86eaaa
Henok Tilahun <henok.tilahun1996@gmail.com>
2024-10-04 09:05:32 -0400

    expand user schema

--- controllers/users.js
+++ controllers/users.js
@@ -11,21 +11,33 @@ const getUsers = (req, res) => {
     .then((users) => res.send(users))
     .catch((err) => {
       console.error(err);
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server" });
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
     });
 };
 
 // POST /user
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
-  User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+  const { name, avatar, email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        User.create({ name, avatar })
+          .then((user) => res.status(201).send(user))
+          .catch((err) => {
+            console.error(err);
+            if (err.name === "ValidationError") {
+              return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+            }
+            return res
+              .status(INTERNAL_SERVER_ERROR)
+              .send({ message: "An error has occurred on the server" });
+          });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server" });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
     });
 };
 
@@ -43,7 +55,34 @@ const getUser = (req, res) => {
       if (err.name === "CastError") {
         return res.status(BAD_REQUEST).send({ message: "Invalid data" });
       }
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server" });
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
+    });
+};
+
+const loginUser = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        // the hashes didn't match, rejecting the promise
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+
+      // authentication successful
+      res.send({ message: "Everything good!" });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
     });
 };
 
--- models/user.js
+++ models/user.js
@@ -18,6 +18,21 @@ const userSchema = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
 });
 
 module.exports = mongoose.model("user", userSchema);
