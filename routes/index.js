e0969309750b96efd95d675f66db1a96875080aa
Henok Tilahun <henok.tilahun1996@gmail.com>
2024-11-04 23:05:28 -0500

    remove duplicate

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,19 @@
 const cors = require("cors");
-const helmet = require("helmet")
+const helmet = require("helmet");
 const router = require("express").Router();
 const userRouter = require("./users");
 const itemRouter = require("./colothingitems");
 const { NOT_FOUND } = require("../utils/errors");
 const { loginUser, createUser } = require("../controllers/users");
 
-router.use(cors());
 router.use(helmet());
 router.use("/items", itemRouter);
 router.post("/signin", loginUser);
 router.post("/signup", createUser);
 router.use("/users", userRouter);
-<<<<<<< HEAD
 router.use(cors());
 
 router.use((req, res) => res.status(NOT_FOUND).send({ message: "Not Found" }));
 // router.use(cors());
-=======
-router.use((req, res) => res.status(NOT_FOUND).send({ message: "Not Found" }));
 
->>>>>>> refs/remotes/origin/main
 module.exports = router;
