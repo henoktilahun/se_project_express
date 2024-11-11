b9f3d881f143ca90dc4800be48b70f3f87880eac
Henok Tilahun <henok.tilahun1996@gmail.com>
2024-11-11 00:20:49 -0500

    move cors to top

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,12 @@ const itemRouter = require("./colothingitems");
 const { NOT_FOUND } = require("../utils/errors");
 const { loginUser, createUser } = require("../controllers/users");
 
+router.use(cors());
 router.use(helmet());
 router.use("/items", itemRouter);
 router.post("/signin", loginUser);
 router.post("/signup", createUser);
 router.use("/users", userRouter);
-router.use(cors());
 
 router.use((req, res) => res.status(NOT_FOUND).send({ message: "Not Found" }));
 // router.use(cors());
