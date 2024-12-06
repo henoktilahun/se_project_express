20f6db6d88aca9b2a258450e9ad7eeb29212d94f
Henok Tilahun <henok.tilahun@raytheon.com>
2024-12-06 14:06:58 -0500

    add server request validation and logging

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
 const {errorHandler} = require("./middlewares/error-handler")
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -14,7 +16,10 @@ mongoose
   .catch(console.error);
 
 app.use(express.json());
+app.use(requestLogger);
 app.use("/", mainRouter);
+app.use(errorLogger);
+app.use(errors())
 app.use(errorHandler)
 
 app.listen(PORT, () => {
